c6d0ac9add33534cb7e304427aebc1a22a717cb8
sebam2k4 <sebam2k4@gmail.com>
2018-09-29 14:31:03 +0000

    handdle data when it's only a single dict and always return a 'response'

--- src/flaskServer/rail_server.py
+++ src/flaskServer/rail_server.py
@@ -8,34 +8,38 @@ import requests
 import xmltodict
 
 app = Flask(__name__)
-CORS(app) # for avoiding cors errors when testing locally
-# cors = CORS(app, resources={r"/rail-data/*": {"origins": "*"}})
 
-@app.route('/stations', methods=['GET', 'POST'])
+cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:4200"]}})
+
+
+@app.route('/api/stations', methods=['GET', 'POST'])
 def get_stations_data():
     stations_xml_api = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML"
     req = requests.get(stations_xml_api)
     data = xmltodict.parse(req.content)
     stations_data = data["ArrayOfObjStation"]["objStation"]
     if stations_data is not None:
-        return jsonify(stations_data)
+        return make_response(jsonify(stations_data), 200)
     return make_response(jsonify({'error': 'Train stations data currently unavailable'}), 404)
     
   
-@app.route('/station-data/<station_code>', methods=['GET'])
+@app.route('/api/station-data/<station_code>', methods=['GET'])
 # @app.route('/station-data', methods=['GET'])
 def get_station_info(station_code):
     station_xml_api = "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode={0}&NumMins=90".format(station_code)
     req = requests.get(station_xml_api)
     data = xmltodict.parse(req.content)
     try:
-        station_data = data["ArrayOfObjStationData"]["objStationData"]
-    	# if only one item available the api will respond with {} and not [{},{},...]
-        # if single object push to empty array and return the array with single object
-        return jsonify(station_data)
+        station_data = data["ArrayOfObjStationData"]["objStationData"] # returns a list
+        # when data contains only 1 it returns a dict so need to put it in a list
+        if isinstance(station_data, dict):
+            a = station_data
+            station_data = []
+            station_data.append(a)
+        return make_response(jsonify(station_data), 200)
     except KeyError:
         print "No trains expected in the next 90 minutes at this station"
-        return jsonify({'info': 'Nothing found'})
+        return make_response(jsonify({'info': 'Nothing found'}), 404)
         
     
 
