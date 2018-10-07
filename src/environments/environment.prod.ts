cbfd849574db38d3af162d9267aaf6fa047944fb
sebam2k4 <sebam2k4@gmail.com>
2018-10-07 00:58:17 +0000

    fix cookie domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   IrishRailAPI_Stations: "https://sebam2k4.pythonanywhere.com/api/stations",
   IrishRailAPI_StationData: "https://sebam2k4.pythonanywhere.com/api/station-data",
-  CookieDomain: 'localhost'
+  CookieDomain: 'stations.ie'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   production: false,
   IrishRailAPI_Stations: "http://127.0.0.1:5000/api/stations",
   IrishRailAPI_StationData: "http://127.0.0.1:5000/api/station-data",
-  CookieDomain: 'stations.ie' 
+  CookieDomain: 'localhost' 
 };
