b4485e43bbc3ab9a14d218238aaf5cbc33e44a4d
sebam2k4 <sebam2k4@gmail.com>
2019-03-17 18:32:42 +0000

    NO-TICKET: reorganize stations component

--- src/modules/stations/stations-view.component.spec.ts
+++ src/modules/stations/stations-view.component.spec.ts
@@ -12,10 +12,10 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { StationsViewComponent } from './stations-view.component';
-import { IrishRailService } from '../../../api/Irish-rail/irish-rail.service';
-import { ApiIrishRailModule } from '../../../api/Irish-rail/irish-rail.module';
+import { IrishRailService } from '../../api/Irish-rail/irish-rail.service';
+import { ApiIrishRailModule } from '../../api/Irish-rail/irish-rail.module';
 
-import { StationsSelectComponent } from './select/stations-select.component';
+import { StationsSelectComponent } from './components/select/stations-select.component';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { MatInputModule, MatSelectModule } from '@angular/material';
 
