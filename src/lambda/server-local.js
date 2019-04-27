e59d6df19e8bf4e5a147336c5080d2d2eb9778b6
Sebastian Kulig <sebam2k4@gmail.com>
2019-04-27 14:16:37 +0100

    GH-45: Add and configure CORS (#53)
    * GH-45: Add and configure CORS
    
    * GH-45: Test CORS
    
    * GH-45: Revert localhost regex

--- src/lambda/server-local.js
+++ src/lambda/server-local.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 
 const app = require('./src/stations-express');
-const open = require('open');
 const config = require('./src/config/config');
 
 app.listen(3000, '127.0.0.1', () => {
-  console.log('Starting local app on port 3000...');
-  console.log(`opening http://127.0.0.1:${config.localDevPort}/${config.functionsPath}/${config.functionName}`);
-  open(`http://127.0.0.1:${config.localDevPort}/${config.functionsPath}/${config.functionName}`);
+  console.log(`Starting local app on port ${config.localDevPort}...`);
+  console.log(`live at http://127.0.0.1:${config.localDevPort}/${config.functionsPath}/${config.functionName}/stations`);
 });
--- src/lambda/src/config/config.js
+++ src/lambda/src/config/config.js
@@ -1,5 +1,20 @@
+const clientAppUrl = 'https://www.stations.ie';
+const localhostRegEx = /https?:\/\/localhost:\d{4}/;
+const netlifyDeployPreviewRegEx = /https:\/\/deploy-preview-\d{1,3}--stations.netlify.com/;
+const netlifyDeployPreviewShaRegEx = /https:\/\/[a-z0-9]{24}--stations.netlify.com/;
+
+const corsWhiteList = [
+  clientAppUrl,
+  localhostRegEx,
+  netlifyDeployPreviewRegEx,
+  netlifyDeployPreviewShaRegEx
+];
+
 module.exports = {
   functionsPath: '.netlify/functions',
   functionName: 'stations-express',
-  localDevPort: 3000
+  localDevPort: 9000,
+  corsOptions: {
+    origin: corsWhiteList
+  }
 };
