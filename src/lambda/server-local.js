d021773c4bf61b882fb5ae2e11903fb8ad9831f6
Sebastian Kulig <sebam2k4@gmail.com>
2019-11-23 23:09:17 +0000

    clean up scripts and dependencies (#61)
    * clean up scripts and dependencies
    
    * fix
    
    * add path for lambda dependencies install

--- src/lambda/server-local.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const app = require('./src/stations-express');
-const config = require('./src/config/config');
-
-app.listen(`${config.localDevPort}`, '127.0.0.1', () => {
-  console.log(`Starting local app on port ${config.localDevPort}...`);
-  console.log(`live at http://127.0.0.1:${config.localDevPort}/${config.functionsPath}/${config.functionName}/stations`);
-});
