2f883b5c5ae4706cd1c9ef731e1e6a923e3aa34a
Sebastian Kulig <sebam2k4@gmail.com>
2020-12-29 11:22:43 +0000

    GH-84: fix deploy preview url

--- lambda/src/config/config.ts
+++ lambda/src/config/config.ts
@@ -1,13 +1,13 @@
 const clientAppUrl = 'https://www.stations.ie';
+const clientAppNetlifyUrl = 'https://stations.netlify.app';
 const localhostUrls = /https?:\/\/localhost:\d{4}/;
-const netlifyDeployPreviewUrls = /https:\/\/deploy-preview-\d{1,3}--stations.netlify.app/;
-const netlifyDeployPreviewShaUrls = /https:\/\/[a-z0-9]{24}--stations.netlify.app/;
+const netlifyDeployPreviewUrls = /^https:\/\/[a-zA-Z0-9-]*--stations\.netlify\.app$/;
 
 const corsWhiteList = [
   clientAppUrl,
+  clientAppNetlifyUrl,
   localhostUrls,
-  netlifyDeployPreviewUrls,
-  netlifyDeployPreviewShaUrls
+  netlifyDeployPreviewUrls
 ];
 
 const corsOptions = {
