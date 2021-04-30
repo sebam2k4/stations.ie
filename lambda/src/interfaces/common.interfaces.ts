eb7cd6e1fa8353a7472f39044f02d7fb1e121201
Sebastian Kulig <sebam2k4@gmail.com>
2021-04-30 14:22:33 +0100

    NO-GH: refactor models and rail service

--- lambda/src/interfaces/common.interfaces.ts
+++ lambda/src/interfaces/common.interfaces.ts
@@ -1,58 +1,47 @@
-export interface Station {
-  stationCode: string;
-  stationFullName: string;
-}
+// eslint-disable-next-line @typescript-eslint/ban-types
+type PublicAndNonMethodKeys<T> = ({[P in keyof T]: T[P] extends Function ? never : P } & { [x: string]: never })[keyof T];  
+type RemovePrivateAndNonMethodKeys<T> = Pick<T, PublicAndNonMethodKeys<T>>;
 
-export class Station2 {
+export class Station {
   public stationCode: string;
   public stationFullName: string;
-  constructor(station: Station) {
-    this.stationCode = station.stationCode;
-    this.stationFullName = station.stationFullName;
+  // private prop2!: number;
+
+  constructor(obj: RemovePrivateAndNonMethodKeys<Station>) {
+    this.stationCode = obj.stationCode;
+    this.stationFullName = obj.stationFullName;
   }
+
+  // private method(): void {}
+
+  // TODO: set any additional props when needed for the UI
 }
 
-
-export interface Journey {
-  destination: string;
-  destinationTime: string;
-  dueIn: number;
-  expectedArrival: string;
-  expectedDeparture: string;
-  late: number;
-  origin: string;
-  originTime: string;
-  scheduledArrival: string;
-  scheduledDeparture: string;
-  stationCode: string;
-  stationFullName: string;
-}
-
-export class Journey2 {
-  destination: string;
-  destinationTime: string;
-  dueIn: number;
-  expectedArrival: string;
-  expectedDeparture: string;
-  late: number;
-  origin: string;
-  originTime: string;
-  scheduledArrival: string;
-  scheduledDeparture: string;
-  stationCode: string;
-  stationFullName: string;
-  constructor(journey: Journey) {
-    this.destination = journey.destination;
-    this.destinationTime = journey.destinationTime;
-    this.dueIn = journey.dueIn;
-    this.expectedArrival = journey.expectedArrival;
-    this.expectedDeparture = journey.expectedDeparture;
-    this.late = journey.late;
-    this.origin = journey.origin;
-    this.originTime = journey.originTime;
-    this.scheduledArrival = journey.scheduledArrival;
-    this.scheduledDeparture = journey.scheduledDeparture;
-    this.stationCode = journey.stationCode;
-    this.stationFullName = journey.stationFullName;
+export class Journey {
+  public destination: string;
+  public destinationTime: string;
+  public dueIn: number;
+  public expectedArrival: string;
+  public expectedDeparture: string;
+  public late: number;
+  public origin: string;
+  public originTime: string;
+  public scheduledArrival: string;
+  public scheduledDeparture: string;
+  public stationCode: string;
+  public stationFullName: string;
+  constructor(obj: RemovePrivateAndNonMethodKeys<Journey>) {
+    this.destination = obj.destination;
+    this.destinationTime = obj.destinationTime;
+    this.dueIn = obj.dueIn;
+    this.expectedArrival = obj.expectedArrival;
+    this.expectedDeparture = obj.expectedDeparture;
+    this.late = obj.late;
+    this.origin = obj.origin;
+    this.originTime = obj.originTime;
+    this.scheduledArrival = obj.scheduledArrival;
+    this.scheduledDeparture = obj.scheduledDeparture;
+    this.stationCode = obj.stationCode;
+    this.stationFullName = obj.stationFullName;
   }
 }
