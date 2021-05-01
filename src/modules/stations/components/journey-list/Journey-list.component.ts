d356c22c0c3350ff4403d213fcd46eeb96a34ec0
Sebastian Kulig <sebam2k4@gmail.com>
2021-05-01 10:47:55 +0100

    GH-14: fix dep times for origin stations (#93)
    closes GH-14

--- src/modules/stations/components/journey-list/Journey-list.component.ts
+++ src/modules/stations/components/journey-list/Journey-list.component.ts
@@ -12,9 +12,25 @@ export class JourneyListComponent {
   @Input() journeysList: IrishRailStationJourney[];
   @Output() refreshJourneys = new EventEmitter<IrishRailStation>();
 
-  public displayedColumns: string[] = ['destination', 'origin', 'scharrival', 'late', 'exparrival'];
+  public displayedColumns: string[] = ['destination', 'origin', 'scheduled', 'late', 'expected'];
 
   public refresh(station: IrishRailStation): void {
     this.refreshJourneys.emit(station);
+  }
+
+  private isOrigin(row: IrishRailStationJourney): boolean {
+    return row.origin === row.stationFullName;
+  }
+
+  private isDestination(row: IrishRailStationJourney): boolean {
+    return row.destination === row.stationFullName;
+  }
+
+  public getExpectedTime(row: IrishRailStationJourney): string {
+    return this.isDestination(row) ? row.expectedArrival : row.expectedDeparture;
+  }
+
+  public getScheduledTime(row: IrishRailStationJourney): string {
+    return this.isDestination(row) ? row.scheduledArrival : row.scheduledDeparture;
   }
 }
